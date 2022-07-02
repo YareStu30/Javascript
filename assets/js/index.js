c38022fc954c0549a6cc0a187aad28c8903a4580
yarenla <yare.cs22@gmail.com>
2022-07-01 20:17:25 -0400

    add container, card,variables and option

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+let button = document.querySelector("#button");document.querySelector("#button").addEventListener("click", () => {
+
+    document.querySelector('p .total').innerHTML = document.querySelector("#precio").value*document.querySelector('#var-1').value;
+    document.querySelector('p .cantidad').innerHTML = document.querySelector('#variable1').value;
+    document.querySelector('p .color_laptopt').style.color = document.querySelector('#variable2').value;
+})
